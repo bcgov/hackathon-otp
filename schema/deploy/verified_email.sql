ae30afdac765a5523e2e2dd31499c326afb772f5
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-06-07 10:58:34 -0700

    refactor: removing is_verifiec

--- schema/deploy/verified_email.sql
+++ schema/deploy/verified_email.sql
@@ -8,9 +8,8 @@ SET client_min_messages = 'warning';
 CREATE TABLE everify.verified_email (
     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     auth_provider_uuid  TEXT NOT NULL,
-    email_address       TEXT        NOT NULL,
-    is_verified         BOOLEAN     NOT NULL    DEFAULT false,
-    verified_at         TIMESTAMPTZ NOT NULL    DEFAULT NOW()
+    email_address       TEXT NOT NULL,
+    verified_at         TIMESTAMPTZ
 );
 
 COMMIT;
