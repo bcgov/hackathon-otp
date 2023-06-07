60d122bbec40243e669c7a46a145d217edc63568
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-06-07 10:43:42 -0700

    chore: sqich conf

--- schema/deploy/otp.sql
+++ schema/deploy/otp.sql
@@ -6,14 +6,10 @@ BEGIN;
 SET client_min_messages = 'warning';
 
 CREATE TABLE everify.otp (
-    id          INTEGER GENERATED ALWAYS AS IDENTITY,
-    email_id    INTEGER,
+    id          INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email_id    INTEGER REFERENCES everify.verified_email(id),
     otp         TEXT,
-    created_at  TIMESTAMPTZ NOT NULL    DEFAULT NOW(),
-    PRIMARY KEY(id),
-    CONSTRAINT fk_email
-        FOREIGN KEY(email_id)
-            REFERENCES everify.verified_email(id)
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 COMMIT;
