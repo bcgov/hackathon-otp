9c3229eabf9c3e7d7614167acb44ad4c7e1c9c69
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2023-06-08 11:45:47 -0700

    feat: updates to the middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,26 +1,37 @@
 const express = require("express");
 const request = require("request");
 
-const config = {
-  everify_host: "...",
-  route_prefix: "everify",
+const defaultConfig = {
+  everifyHost: "localhost:8000",
+  routePrefix: "/everify",
 };
 
-const getMiddleware = (config) => {
-  const router = express.Router();
+function getMiddleware(configuration) {
+  const config = configuration || defaultConfig;
+
+  // Internal router to forward to the API
+  const internalRouter = express.Router();
 
   // Handle submit form endpoint with OTP
-  router.use("/verify", (req, res, next) => {});
+  internalRouter.post("/verify", (req, res, next) => {
+    console.log("Verify!");
+  });
 
   // Handle static assets for verify page
-  router.use("/static/:filename", (req, res) => {
+  internalRouter.get("/static/:filename", (req, res) => {
     request({
-      uri: `${everify_host}/static/${req.params.filename}`,
+      uri: `${config.everifyHost}/static/${req.params.filename}`,
     }).pipe(res);
   });
 
+  // Main middleware
+  const router = express.Router();
+  router.use(config.routePrefix, internalRouter);
+
   router.use(async (req, res, next) => {
-    const isVerifiedUrl = new URL(`${everify_host}/is_verified`);
+    if (!req.claims) next();
+
+    const isVerifiedUrl = new URL(`${config.everifyHost}/is_verified`);
     isVerifiedUrl.searchParams.append(
       "email_address",
       req.claims.email_address
@@ -34,7 +45,7 @@ const getMiddleware = (config) => {
     }
 
     // Send email
-    const sendEmailUrl = new URL(`${everify_host}/create_otp`);
+    const sendEmailUrl = new URL(`${config.everifyHost}/create_otp`);
     const sendEmailResponse = await fetch(sendEmailUrl, {
       method: "POST",
       body: {
@@ -42,11 +53,12 @@ const getMiddleware = (config) => {
         auth_provider_uuid: req.claims.uuid,
       },
     });
-    if (!sendEmailResponse.ok) throw "error calling send email endpoint";
+    if (!sendEmailResponse.ok)
+      throw new Error("error calling send email endpoint");
 
     // Show the user the OTP page
-    const verifyPageUrl = new URL(`${everify_host}/is_verified`);
-    verifyPageUrl.searchParams.append("route_prefix", "everify");
+    const verifyPageUrl = new URL(`${config.everifyHost}/is_verified`);
+    verifyPageUrl.searchParams.append("route_prefix", config.route_prefix);
     verifyPageUrl.searchParams.append(
       "email_address",
       req.claims.email_address
@@ -57,7 +69,11 @@ const getMiddleware = (config) => {
       url: verifyPageUrl,
     }).pipe(res);
   });
-};
+
+  return router;
+}
+
+module.exports = { getMiddleware, defaultConfig };
 
 /// consumer would do
-// app.use('/everify', getMiddleware())
+// app.use(package.getMiddleware( {..optional config..} ))
